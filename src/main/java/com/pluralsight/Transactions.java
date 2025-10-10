b6f3b9942ab0ef31099207ae4276d9fa066eb37e
Kylee <kkeith.biz@gmail.com>
2025-10-10 12:06:22 -0400

    Transactions Getters and Setter First two methods started

--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,61 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transactions {
+    //date|time|description|vendor|amount
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private double amount;
+
+    public Transactions(LocalDate date, LocalTime time, String description, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+    public String toString() {
+        return "Transactions{" +
+                "date=" + date +
+                ", time=" + time +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
---
+++ src/main/java/com/pluralsight/VaultBook.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class VaultBook {
+    public static void main(String[] args) {
+        run();
+    }
+    public static void run() {
+        System.out.println("""
+==========================================
+          WELCOME TO VAULT BOOK
+==========================================
+   Your trusted Accounting Ledger System
+   Secure. Simple. Smart.
+------------------------------------------
+""");
+        System.out.println();
+
+        while (true)
+        {
+            homeScreen();
+            menuSelector();
+        }
+    }
+    public static void homeScreen() {
+
+    }
+    public static void menuSelector() {
+
+    }
+}
